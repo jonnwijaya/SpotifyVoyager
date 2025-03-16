6c97fd55594be860e7a86f8524b8e3f07bc9b443
jonnwijaya <jonnwijaya@users.noreply.github.com>
2025-03-17 03:01:47 +0700

    fixed callback page.js

--- app/callback/page.js
+++ app/callback/page.js
@@ -79,7 +79,7 @@ export default function Callback() {
     handleCallback();
 
     return () => clearInterval(progressInterval);
-  }, [router]);
+  }, [loading, router]);
 
   async function fetchUserProfile(accessToken) {
     try {
